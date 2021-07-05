1e004c6c6189691085bce11d75fbe08b3d2df00a
Luiz M. Faria <maltezfaria@gmail.com>
2021-07-05 10:06:45 +0200

    test hilbert curve

---
+++ test/hilbertcurve_test.jl
@@ -0,0 +1,15 @@
+using HMatrices
+using HMatrices: hilbert_linear_to_cartesian, hilbert_cartesian_to_linear
+
+@testset "Hilbert points" begin
+    # chech that hilbert cuves spans the lattice, and the invere is correct
+    for n in [2^p for p in 0:3]
+        lattice = Set((i,j) for i in 0:n-1,j in 0:n-1)
+        for d in 0:(n^2-1)
+            x,y = hilbert_linear_to_cartesian(n,d)
+            pop!(lattice,(x,y))
+            @test hilbert_cartesian_to_linear(n,x,y) == d
+        end
+        @test isempty(lattice)
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using SafeTestsets
 
 @safetestset "Hyperrectangle" begin include("hyperrectangle_test.jl") end
 
+@safetestset "Hilbert curve" begin include("hilbertcurve_test.jl") end
+
 @safetestset "Clustertree" begin include("clustertree_test.jl") end
 
 @safetestset "Lowrank matrices" begin include("lowrankmatrices_test.jl") end
