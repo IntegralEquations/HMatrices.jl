a812099e5309d1cc6161a7622641ce167b44ac3f
Luiz M. Faria <maltezfaria@gmail.com>
2023-11-05 19:41:11 +0100

    remove dead code from `hyperrectangle`

--- src/hyperrectangle.jl
+++ src/hyperrectangle.jl
@@ -13,20 +13,8 @@ HyperRectangle(l::SVector, h::SVector) = HyperRectangle(promote(l, h)...)
 # 1d case
 HyperRectangle(a::Number, b::Number) = HyperRectangle(SVector(a), SVector(b))
 
-# Base.eltype(::HyperRectangle{N,T}) where {N,T} = T
-# ambient_dimension(::HyperRectangle{N}) where {N} = N
-# geometric_dimension(::HyperRectangle{N}) where {N} = N
 low_corner(r::HyperRectangle) = r.low_corner
 high_corner(r::HyperRectangle) = r.high_corner
-
-function Base.isapprox(h1::HyperRectangle, h2::HyperRectangle; kwargs...)
-    return isapprox(h1.low_corner, h2.low_corner; kwargs...) &&
-           isapprox(h1.high_corner, h2.high_corner; kwargs...)
-end
-
-width(r::HyperRectangle) = high_corner(r) - low_corner(r)
-
-half_width(r::HyperRectangle) = width(r) / 2
 
 """
     distance(Ω1,Ω2)
@@ -46,22 +34,6 @@ function distance(rec1::HyperRectangle{N}, rec2::HyperRectangle{N}) where {N}
     end
     return sqrt(d2)
 end
-
-"""
-    distance(x::SVector,r::HyperRectangle)
-
-The (minimal) Euclidean distance between the point `x` and any point `y ∈ r`.
-"""
-function distance(pt::SVector{N}, rec::HyperRectangle{N}) where {N}
-    d2 = 0
-    rec_low_corner = low_corner(rec)
-    rec_high_corner = high_corner(rec)
-    for i in 1:N
-        d2 += max(0, pt[i] - rec_high_corner[i])^2 + max(0, rec_low_corner[i] - pt[i])^2
-    end
-    return sqrt(d2)
-end
-distance(rec::HyperRectangle{N}, pt::SVector{N}) where {N} = distance(pt, rec)
 
 """
     diameter(Ω)
@@ -85,33 +57,6 @@ Center of the smallest ball containing `Ω`.
 center(r::HyperRectangle) = (low_corner(r) + high_corner(r)) / 2
 
 Base.in(point, h::HyperRectangle) = all(low_corner(h) .<= point .<= high_corner(h))
-
-function vertices(rec::HyperRectangle{2})
-    lc = low_corner(rec)
-    hc = high_corner(rec)
-    return SVector(
-        SVector(lc[1], lc[2]),
-        SVector(hc[1], lc[2]),
-        SVector(hc[1], hc[2]),
-        SVector(lc[1], hc[2]),
-    )
-end
-function vertices(rec::HyperRectangle{3})
-    lc = low_corner(rec)
-    hc = high_corner(rec)
-    return SVector(
-        # lower face
-        SVector(lc[1], lc[2], lc[3]),
-        SVector(hc[1], lc[2], lc[3]),
-        SVector(hc[1], hc[2], lc[3]),
-        SVector(lc[1], hc[2], lc[3]),
-        # upper face
-        SVector(lc[1], lc[2], hc[3]),
-        SVector(hc[1], lc[2], hc[3]),
-        SVector(hc[1], hc[2], hc[3]),
-        SVector(lc[1], hc[2], hc[3]),
-    )
-end
 
 function bounding_box(els, cube = false)
     isempty(els) && (error("data cannot be empty"))
